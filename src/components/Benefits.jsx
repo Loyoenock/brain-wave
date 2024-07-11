65396f18da72a74d5f0b3aa57b73bc173736e87a
loyoenock <loyoenock@gmail.com>
2024-07-11 14:44:35 +0300

    Benefit section - Heading component added

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+    return (
+        <Section id="features">
+            <div className="container relative z-2">
+                <Heading className="md:max-w-md lg:max-w-2xl" title="Chat Smatter, Not Harder with Brainwave" />
+            </div>
+        </Section>
+    )
+};
+export default Benefits;
