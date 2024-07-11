6f8a8ecbb2343029ff2aca52ed36c481595fb21d
loyoenock <loyoenock@gmail.com>
2024-07-11 09:13:12 +0300

    Generating component added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import { BackgroundCircles, BottomLine, Gradient } from "../components/design/He
 import { heroIcons } from "../constants"
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
 
 const Hero = () => {
     const parallaxRef = useRef(null);
@@ -18,8 +19,8 @@ const Hero = () => {
             >
             <div className="container relative" ref={parallaxRef}>
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
-                    <h1 className="h1 mb-6">Explore the possibilitis of AI Chatting with  
-                    <span className="inline-block relative">Brainwave{""} <img src={curve} className="absolute top-full left-0 w-full xl:-mt-2"  width={624} height={28} alt="curve"/></span>
+                    <h1 className="h1 mb-6">Explore the possibilitis of &nbsp;AI&nbsp;Chatting with  
+                    <span className="inline-block relative">Brainwave{" "} <img src={curve} className="absolute top-full left-0 w-full xl:-mt-2"  width={624} height={28} alt="curve"/></span>
                     </h1>
                     <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
                     Unleash the power of AI within Brainwave. Upgrade your productivity
@@ -35,6 +36,8 @@ const Hero = () => {
                             <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
                             <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                                 <img src={robot} className="w-full scale-[1.7]translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]" width={1024} height={490} alt="AI" />
+                                
+                                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
                                 <ScrollParallax isAbsolutelyPositioned>
                                     <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/10 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
@@ -55,7 +58,8 @@ const Hero = () => {
                     </div>
                     <BackgroundCircles />
                 </div>
-            </div>           
+            </div> 
+            <BottomLine />          
         </Section>
     )
 };
