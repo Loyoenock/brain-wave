22b30fa8357d0f090a5799680a83f8f44f1f6cee
loyoenock <loyoenock@gmail.com>
2024-07-11 08:29:47 +0300

    Components - Scroll parallax, Gradient and Background circles added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "../components/design/Hero"
+import { heroIcons } from "../constants"
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 
 const Hero = () => {
+    const parallaxRef = useRef(null);
     return (
         <Section
             className="pt-[12rem] -mt-[5.25rem]"
@@ -11,10 +16,10 @@ const Hero = () => {
             customPaddings
             id="hero"
             >
-            <div className="container relative">
+            <div className="container relative" ref={parallaxRef}>
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
-                    <h1 className="h1 mb-6">Explore the possibilitis of AI Chatting with Brainwave{""} 
-                    <span className="inline-block relative">Brainwave <img src={curve} className="absolute top-full left-0 w-full xl:-mt-2"  width={624} height={28} alt="curve"/></span>
+                    <h1 className="h1 mb-6">Explore the possibilitis of AI Chatting with  
+                    <span className="inline-block relative">Brainwave{""} <img src={curve} className="absolute top-full left-0 w-full xl:-mt-2"  width={624} height={28} alt="curve"/></span>
                     </h1>
                     <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
                     Unleash the power of AI within Brainwave. Upgrade your productivity
@@ -30,12 +35,25 @@ const Hero = () => {
                             <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
                             <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                                 <img src={robot} className="w-full scale-[1.7]translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]" width={1024} height={490} alt="AI" />
+
+                                <ScrollParallax isAbsolutelyPositioned>
+                                    <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/10 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                                        {heroIcons.map((icon, index) => (
+                                            <li className="p-5 key={index}">
+                                                <img src={icon} width={24} height={25} alt={icon} />
+                                            </li>
+                                        ))}
+                                    </ul>
+
+                                </ScrollParallax>
                             </div>
                         </div>
+                        <Gradient />
                     </div>
                     <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
                         <img src={heroBackground} width={1440} height={1800} alt="hero"  />
                     </div>
+                    <BackgroundCircles />
                 </div>
             </div>           
         </Section>
